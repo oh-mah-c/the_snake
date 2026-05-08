c36c32139a86b99670088750a6d8a87f4aa5449e
pomegrante <vanhaminhquan2406@gmail.com>
2026-05-08 20:58:08 +0700

    Update sdl rendering

--- engine.c
+++ engine.c
@@ -1,132 +1,143 @@
-#include <stdio.h>
-#include <conio.h>
+#include <SDL.h>
+#include <stdlib.h>
 #include "engine.h"
+
+#define TILE_SIZE 24
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+void cleanup_graphics()
+{
+    if (renderer)
+        SDL_DestroyRenderer(renderer);
+    if (window)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 void init_console()
 {
-    printf("\033[?25l\033[H\033[J");
-    fflush(stdout);
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("The Snake",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE,
+                              SDL_WINDOW_SHOWN);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    atexit(cleanup_graphics);
 }
 
 void render_frame(Snake *snake, Point *food, SpecialFood *s_food, int map[HEIGHT][WIDTH], int score)
 {
-    char buffer[WIDTH * HEIGHT * 100];
-    int pos = 0;
+    SDL_SetRenderDrawColor(renderer, 20, 24, 30, 255);
+    SDL_RenderClear(renderer);
 
-    const char *head_color = "\033[1;32m";
-    const char *body_color = "\033[0;32m";
+    SDL_Rect rect;
+    rect.w = TILE_SIZE;
+    rect.h = TILE_SIZE;
 
-    if (snake->ghost_steps > 0)
-    {
-        head_color = "\033[1;36m";
-        body_color = "\033[0;36m";
-    }
-    else if (snake->poison_timer > 0)
-    {
-        head_color = "\033[1;35m";
-        body_color = "\033[0;35m";
-    }
-
+    SDL_SetRenderDrawColor(renderer, 40, 80, 150, 255);
     for (int y = 0; y < HEIGHT; y++)
     {
         for (int x = 0; x < WIDTH; x++)
         {
             if (map[y][x] == 1)
             {
-                pos += sprintf(&buffer[pos], "\033[1;34m#\033[0m");
-            }
-            else if (x == food->x && y == food->y)
-            {
-                pos += sprintf(&buffer[pos], "\033[1;31m■\033[0m");
-            }
-            else if (s_food->active && x == s_food->pos.x && y == s_food->pos.y)
-            {
-                if (s_food->type == FOOD_GOLD)
-                    pos += sprintf(&buffer[pos], "\033[1;33m$\033[0m");
-                else if (s_food->type == FOOD_POISON)
-                    pos += sprintf(&buffer[pos], "\033[1;35mx\033[0m");
-                else if (s_food->type == FOOD_GHOST)
-                    pos += sprintf(&buffer[pos], "\033[1;36m#\033[0m");
-            }
-
-            else
-            {
-                int is_body = 0;
-                for (int i = 0; i < snake->length; i++)
-                {
-                    if (snake->body[i].x == x && snake->body[i].y == y)
-                    {
-                        if (i == 0)
-                        {
-                            pos += sprintf(&buffer[pos], "\033[1;32mO\033[0m");
-                        }
-                        else
-                        {
-                            pos += sprintf(&buffer[pos], "\033[0;32mo\033[0m");
-                        }
-
-                        is_body = 1;
-                        break;
-                    }
-                }
-
-                if (!is_body)
-                {
-                    pos += sprintf(&buffer[pos], " ");
-                }
+                rect.x = x * TILE_SIZE;
+                rect.y = y * TILE_SIZE;
+                SDL_RenderFillRect(renderer, &rect);
             }
         }
-        buffer[pos++] = '\n';
     }
 
-    pos += sprintf(&buffer[pos], "\033[1;33mScore: %d\033[0m\n", score);
-    if (snake->ghost_steps > 0)
-        pos += sprintf(&buffer[pos], "\033[1;36m[GHOST: %d steps]\033[0m  ", snake->ghost_steps);
-    if (snake->poison_timer > 0)
-        pos += sprintf(&buffer[pos], "\033[1;35m[POISONED: %d tick]\033[0m  ", snake->poison_timer);
-    if (s_food->active && s_food->type == FOOD_GOLD)
-        pos += sprintf(&buffer[pos], "\033[1;33m[Gold disapear: %d]\033[0m", s_food->ttl);
+    SDL_SetRenderDrawColor(renderer, 220, 50, 50, 255);
+    rect.x = food->x * TILE_SIZE;
+    rect.y = food->y * TILE_SIZE;
+    SDL_RenderFillRect(renderer, &rect);
 
-    buffer[pos++] = '\n';
-    buffer[pos] = '\0';
+    if (s_food->active)
+    {
+        if (s_food->type == FOOD_GOLD)
+            SDL_SetRenderDrawColor(renderer, 255, 215, 0, 255);
+        else if (s_food->type == FOOD_POISON)
+            SDL_SetRenderDrawColor(renderer, 150, 50, 200, 255);
+        else if (s_food->type == FOOD_GHOST)
+            SDL_SetRenderDrawColor(renderer, 0, 250, 250, 255);
 
-    printf("\033[H");
-    fputs(buffer, stdout);
-    fflush(stdout);
+        rect.x = s_food->pos.x * TILE_SIZE;
+        rect.y = s_food->pos.y * TILE_SIZE;
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    for (int i = 0; i < snake->length; i++)
+    {
+        if (i == 0)
+        {
+            if (snake->ghost_steps > 0)
+                SDL_SetRenderDrawColor(renderer, 50, 255, 255, 255);
+            else if (snake->poison_timer > 0)
+                SDL_SetRenderDrawColor(renderer, 200, 80, 255, 255);
+            else
+                SDL_SetRenderDrawColor(renderer, 80, 255, 80, 255);
+        }
+        else
+        {
+            if (snake->ghost_steps > 0)
+                SDL_SetRenderDrawColor(renderer, 0, 150, 150, 255);
+            else if (snake->poison_timer > 0)
+                SDL_SetRenderDrawColor(renderer, 130, 30, 180, 255);
+            else
+                SDL_SetRenderDrawColor(renderer, 0, 180, 0, 255);
+        }
+
+        SDL_Rect body_rect = {
+            (snake->body[i].x * TILE_SIZE) + 1,
+            (snake->body[i].y * TILE_SIZE) + 1,
+            TILE_SIZE - 2,
+            TILE_SIZE - 2};
+
+        SDL_RenderFillRect(renderer, &body_rect);
+    }
+
+    SDL_RenderPresent(renderer);
 }
 
 int get_input(int current_dir)
 {
-    if (_kbhit())
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
     {
-        int ch = _getch();
-        if (ch == 0 || ch == 224)
+        if (event.type == SDL_QUIT)
         {
-            ch = _getch();
+            exit(0);
         }
 
-        switch (ch)
+        if (event.type == SDL_KEYDOWN)
         {
-        case 'w':
-        case 72:
-            if (current_dir != 2)
-                return 0;
-            break;
-        case 'd':
-        case 77:
-            if (current_dir != 3)
-                return 1;
-            break;
-        case 's':
-        case 80:
-            if (current_dir != 0)
-                return 2;
-            break;
-        case 'a':
-        case 75:
-            if (current_dir != 1)
-                return 3;
-            break;
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_w:
+            case SDLK_UP:
+                if (current_dir != 2)
+                    return 0;
+                break;
+            case SDLK_d:
+            case SDLK_RIGHT:
+                if (current_dir != 3)
+                    return 1;
+                break;
+            case SDLK_s:
+            case SDLK_DOWN:
+                if (current_dir != 0)
+                    return 2;
+                break;
+            case SDLK_a:
+            case SDLK_LEFT:
+                if (current_dir != 1)
+                    return 3;
+                break;
+            }
         }
     }
 
